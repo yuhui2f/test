efc62abd47705acec1f928d9459efb1d6cb4893e
yuhui2f <liangdongcf@gmail.com>
2013-08-07 14:41:49 +0800

    test commit php file !

---
+++ brandads.cache.php
@@ -0,0 +1,180 @@
+<?php
+
+//Brand cache file, DO NOT modify me!
+//Created: Aug 1, 2013, 19:23
+
+if(!defined('IN_BRAND')) exit('Access Denied');
+
+$_G['brandads']=array (
+  'ads_show_type' => 'topic',
+  'autonum' => '0',
+  'banner' => '&lt;div&gt;abc&lt;/div&gt;',
+  '' => NULL,
+  'consumearr' => 
+  array (
+    0 => 
+    array (
+      'itemid' => 59,
+      'shopid' => '64',
+    ),
+  ),
+  'consume' => '59',
+  'discountarr' => 
+  array (
+  ),
+  'discount' => '',
+  'enableconsume' => '1',
+  'enablediscount' => '1',
+  'enablegroupbuy' => '1',
+  'enablehotgoods' => '1',
+  'groupbuyarr' => 
+  array (
+  ),
+  'groupbuy' => '',
+  'hotgoodsarr' => 
+  array (
+    0 => 
+    array (
+      'itemid' => 345,
+      'shopid' => false,
+    ),
+    1 => 
+    array (
+      'itemid' => 363,
+      'shopid' => '12',
+    ),
+    2 => 
+    array (
+      'itemid' => 364,
+      'shopid' => '233',
+    ),
+    3 => 
+    array (
+      'itemid' => 360,
+      'shopid' => '152',
+    ),
+    4 => 
+    array (
+      'itemid' => 359,
+      'shopid' => false,
+    ),
+    5 => 
+    array (
+      'itemid' => 349,
+      'shopid' => '222',
+    ),
+    6 => 
+    array (
+      'itemid' => 303,
+      'shopid' => '175',
+    ),
+    7 => 
+    array (
+      'itemid' => 331,
+      'shopid' => '178',
+    ),
+    8 => 
+    array (
+      'itemid' => 296,
+      'shopid' => '161',
+    ),
+    9 => 
+    array (
+      'itemid' => 323,
+      'shopid' => '197',
+    ),
+  ),
+  'hotgoods' => '345,363,364,360,359,349,303,331,296,323',
+  'hotshoparr' => 
+  array (
+  ),
+  'hotshop' => '',
+  'sidebarconsumearr' => 
+  array (
+    0 => 
+    array (
+      'itemid' => 90,
+      'shopid' => '258',
+    ),
+    1 => 
+    array (
+      'itemid' => 91,
+      'shopid' => '290',
+    ),
+    2 => 
+    array (
+      'itemid' => 92,
+      'shopid' => '79',
+    ),
+    3 => 
+    array (
+      'itemid' => 94,
+      'shopid' => '307',
+    ),
+    4 => 
+    array (
+      'itemid' => 95,
+      'shopid' => '12',
+    ),
+  ),
+  'sidebarconsume' => '90,91,92,94,95',
+  'sidebardiscountshoparr' => 
+  array (
+  ),
+  'sidebardiscountshop' => '',
+  'sidebargoodsarr' => 
+  array (
+  ),
+  'sidebargoods' => '',
+  'sidebargroupbuyarr' => 
+  array (
+  ),
+  'sidebargroupbuy' => '',
+  'sidebarshoparr' => 
+  array (
+    0 => 
+    array (
+      'itemid' => 12,
+    ),
+    1 => 
+    array (
+      'itemid' => 14,
+    ),
+    2 => 
+    array (
+      'itemid' => 165,
+    ),
+    3 => 
+    array (
+      'itemid' => 164,
+    ),
+    4 => 
+    array (
+      'itemid' => 140,
+    ),
+  ),
+  'sidebarshop' => '12,14,165,164,140',
+  'topic' => 
+  array (
+    0 => 
+    array (
+      'image' => 'static/images/banner1.jpg',
+      'url' => 'http://www.jnyyw.cn/zhuanti/yixingqi/',
+      'title' => '《壹星期》有奖答题活动入口',
+    ),
+    1 => 
+    array (
+      'image' => 'static/images/banner2.jpg',
+      'url' => 'http://www.jnyyw.cn/zhuanti/zhengxing/',
+      'title' => '都市女报夏日整形主题月',
+    ),
+    2 => 
+    array (
+      'image' => 'static/images/banner3.jpg',
+      'url' => 'http://www.jnyyw.cn/xplus/poll.php?id=1',
+      'title' => '最浪漫婚姻评选大赛投票',
+    ),
+  ),
+); 
+
+?>
